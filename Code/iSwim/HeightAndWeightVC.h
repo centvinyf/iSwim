a78d3f650352e952a9d387fe3863a1038255dd86
MagicBeans2 <magicbeans2@MagicBeans2deMac-mini.local>
2015-01-27 18:36:47 +0800

    1.add some UIs

---
+++ Code/iSwim/HeightAndWeightVC.h
@@ -0,0 +1,13 @@
+//
+//  HeightAndWeightVC.h
+//  iSwim
+//
+//  Created by MagicBeans2 on 15/1/27.
+//  Copyright (c) 2015年 Magic Beans. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HeightAndWeightVC : UIViewController
+
+@end
